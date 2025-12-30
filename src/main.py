6b23dfb55b690e283ff56ba201d09066844f2599
peype74 <peype74@gmail.com>
2025-12-30 16:53:17 +0700

    Connect Huffman module to main

--- src/main.py
+++ src/main.py
@@ -1,1 +1,10 @@
-print("Hello Compression Project")
+from huffman import huffman_compress
+
+# contoh teks uji
+text = "ini adalah contoh teks untuk kompresi huffman"
+
+encoded, codes = huffman_compress(text)
+
+print("Teks asli       :", text)
+print("Teks terkompres :", encoded)
+print("Kode Huffman    :", codes)
