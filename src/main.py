fde43c33286197ea572c0a713c2d21c86d9e7d87
peype74 <peype74@gmail.com>
2025-12-30 17:18:01 +0700

    Integrate DCT transform coding into main program
    Add transform coding analysis using DCT.

--- src/main.py
+++ src/main.py
@@ -1,16 +1,23 @@
 from huffman import huffman_compress
 from lzw import lzw_compress
 from metrics import entropy
+from dct_text import dct_text_compress
 
 text = "ini adalah contoh teks untuk kompresi data"
 
 print("Entropy sumber:", entropy(text))
 
+# Huffman
 encoded_huff, _ = huffman_compress(text)
-encoded_lzw = lzw_compress(text)
-
 print("\n=== HUFFMAN ===")
-print("Hasil encoding:", encoded_huff)
+print(encoded_huff)
 
+# LZW
+encoded_lzw = lzw_compress(text)
 print("\n=== LZW ===")
-print("Hasil encoding:", encoded_lzw)
+print(encoded_lzw)
+
+# DCT
+dct_result = dct_text_compress(text)
+print("\n=== DCT ===")
+print(dct_result)
